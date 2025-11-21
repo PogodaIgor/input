7d68339d4297e1b1a70da0c52f2270d8ae68265e
PogodaIgor <igor.pogoda2010@wp.pl>
2025-11-21 14:57:49 +0100

    a

---
+++ app.js
@@ -0,0 +1,9 @@
+let liczba
+
+function zmien(){
+    if(liczba > 10){
+        document.querySelector("body").style.backgroundColor = "red"
+    }else{
+        document.querySelector("body").style.backgroundColor = "green"
+    }
+}
