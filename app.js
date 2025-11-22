7de1ea709ad835be0344992e78a0abc627141010
PogodaIgor <igor.pogoda2010@wp.pl>
2025-11-22 01:23:51 +0100

    a

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-let liczba
-
-function zmien(){
-    if(liczba > 10){
-        document.querySelector("body").style.backgroundColor = "red"
-    }else{
-        document.querySelector("body").style.backgroundColor = "green"
-    }
+function zmien() {
+  var liczba = document.getElementById("liczba").value;
+  if (liczba < 10) {
+    document.getElementById("body").style.backgroundColor = "red";
+  } else {
+    document.getElementById("body").style.backgroundColor = "green";
+  }
+}
+function oblicz() {
+  var liczba = parseInt(document.getElementById("licz").value);
+  var procent = parseInt(document.getElementById("procent").value);
+  var wynik = liczba + liczba * (procent / 100);
+  document.getElementById("wynik").innerHTML = wynik;
 }
